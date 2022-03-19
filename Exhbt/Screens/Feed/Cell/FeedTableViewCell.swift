8a726a706ed22dbd5f06dd686c92642ed6d525a5
Mehmet Tarhan <hi@memtarhan.com>
2022-03-19 17:21:44 +0000

    added card view

--- Exhbt/Screens/Feed/Cell/FeedTableViewCell.swift
+++ Exhbt/Screens/Feed/Cell/FeedTableViewCell.swift
@@ -12,16 +12,28 @@ class FeedTableViewCell: UITableViewCell {
     static let reuseIdentifier = "Feed"
     static let nibIdentifier = "FeedTableViewCell"
 
+    @IBOutlet var statusContainerView: UIView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        statusContainerView.layer.cornerRadius = 20
+        statusContainerView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 }
 
 class FeedsTableViewDiffableDataSource: UITableViewDiffableDataSource<String?, FeedEntity.Feed.ViewModel> {}
+
+class CardView: UIView {
+    override func layoutSubviews() {
+        layer.cornerRadius = 20.0
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        layer.shadowRadius = 2.0
+        layer.shadowOpacity = 0.7
+    }
+}
